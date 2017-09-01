3372031449b643e93876d12a856b0ed8989d1f30
Simon BERNARD <sbernard.prestataire2@voyages-sncf.com>
2017-09-01 09:39:14 +0200

    (feat) Set up basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { SearchModule } from './search/search.module';
+
+import { appRoutes } from "./app.routing";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: true }
+    ),
+
+    HomeModule,
+    SearchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home/home.component";
+
+export const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  { path: '**', redirectTo: '' }
+];
